121807214b3dc5faf9d33e839729047370609b19
enunsnv <seoej0315@naver.com>
2024-08-03 09:23:20 +0900

    💄style: 뒤로 가기 버튼 위치 수정

---
+++ components/BackButton.tsx
@@ -0,0 +1,27 @@
+// BackButton.js
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
+
+const BackButton = ({ navigation }) => {
+  return (
+    <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+      <Image source={require('../assets/goback.png')} style={styles.backButtonImage} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  backButton: {
+    padding: 5,
+    position: 'absolute',
+    top: 50,
+    left: 20,
+    zIndex: 1,
+  },
+  backButtonImage: {
+    width: 24,
+    height: 24,
+  },
+});
+
+export default BackButton;
