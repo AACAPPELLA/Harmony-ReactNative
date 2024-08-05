9d2dd72826ea854ec0de790cd7d9235acd19e4d8
enunsnv <seoej0315@naver.com>
2024-08-05 17:12:03 +0900

    💄 style: 수정 사항 스타일 통합

--- screens/login/login.tsx
+++ screens/login/login.tsx
@@ -1,54 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import api from '../../axios';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 const Login = ({ navigation }) => {
-  const [serialId, setSerialId] = useState('');
-  const [password, setPassword] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const handleFindPWPress = () => {
-    navigation.navigate('findPW');
-  }
-
-  const storeTokens = async (accessToken, refreshToken) => {
-    try {
-      await AsyncStorage.setItem('accessToken', accessToken);
-      await AsyncStorage.setItem('refreshToken', refreshToken);
-      console.log('토큰 저장 성공');
-    } catch (error) {
-      console.error('토큰 저장 오류', error);
-    }
-  };
-
-  const handleLogin = async () => {
-    try {
-      console.log('로그인 시도 중:', { serialId, password });
-      const response = await api.post('/auth/login', null, {
-        params: {
-          serialId,
-          password
-        }
-      });
-
-      console.log('응답:', response.data);
-
-      if (response.data.success) {
-        const { accessToken, refreshToken } = response.data.data;
-        // 토큰 저장
-        await storeTokens(accessToken, refreshToken);
-        Alert.alert('로그인 성공', '성공적으로 로그인되었습니다.');
-        navigation.navigate('Home');
-      } else {
-        Alert.alert('로그인 실패', '잘못된 아이디 또는 비밀번호입니다.');
-      }
-    } catch (error) {
-      console.error('로그인 오류', error);
-      Alert.alert('로그인 오류', '로그인 중 오류가 발생했습니다. 다시 시도해 주세요.');
-    }
-  };
 
   return (
     <View style={styles.container}>
@@ -60,8 +14,6 @@ const Login = ({ navigation }) => {
         <TextInput 
           placeholder="아이디"
           style={styles.input}
-          value={serialId}
-          onChangeText={setSerialId}
         />
       </View>
       
@@ -71,8 +23,6 @@ const Login = ({ navigation }) => {
           placeholder="비밀번호"
           secureTextEntry={!passwordVisible}
           style={styles.input}
-          value={password}
-          onChangeText={setPassword}
         />
         <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
           <Image 
@@ -84,20 +34,14 @@ const Login = ({ navigation }) => {
 
       <TouchableOpacity 
         style={styles.loginButton}
-        onPress={handleLogin}
+        onPress={() => navigation.navigate('Home')}
       >
         <Text style={styles.loginButtonText}>로그인</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity 
-        style={styles.signupButton}
-        onPress={() => navigation.navigate('SignUp')}
-      >
+      <TouchableOpacity style={styles.signupButton}>
         <Text style={styles.signupButtonText}>회원가입</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleFindPWPress} style={styles.findPWButton}>
-            <Text style={styles.findPWButtonText}>비밀번호를 잊어버리셨나요?</Text>
-          </TouchableOpacity>
     </View>
   );
 };
@@ -154,6 +98,7 @@ const styles = StyleSheet.create({
   loginButtonText: {
     color: '#fff',
     fontSize: 18,
+    padding: 6,
   },
   signupButton: {
     borderWidth: 1,
@@ -167,16 +112,7 @@ const styles = StyleSheet.create({
   signupButtonText: {
     color: '#291695',
     fontSize: 18,
-  },
-  findPWButton: {
-    marginTop: 20,
-    alignItems: 'flex-end',
-    marginRight: 20,
-  },
-  findPWButtonText: {
-    fontSize: 14,
-    color: '#5A5A5A',
-    textDecorationLine: 'underline',
+    padding: 6,
   },
 });
 
